1cdec05862b76d0921fe097d00c19473739aee0f
Ben Lesh <blesh@netflix.com>
2015-04-07 14:53:24 -0700

    upgrade ember-cli to 0.2.3, upgrade rxjs to 2.5.X

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -3,20 +3,4 @@ import {
   setResolver
 } from 'ember-qunit';
 
-// Backfill for Phantom.js / JSCore
-if (!Function.prototype.bind) {
-  Function.prototype.bind = function (context) {
-    var self = this;
-    return function () {
-      return self.apply(context, arguments);
-    };
-  };
-}
-
 setResolver(resolver);
-
-document.write('<div id="ember-testing-container"><div id="ember-testing"></div></div>');
-
-QUnit.config.urlConfig.push({ id: 'nocontainer', label: 'Hide container' });
-var containerVisibility = QUnit.urlParams.nocontainer ? 'hidden' : 'visible';
-document.getElementById('ember-testing-container').style.visibility = containerVisibility;
